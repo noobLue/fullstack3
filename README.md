06cba986ccb4be524cc6093e173652824fcc46e3
nooblue <null>
2024-03-15 19:52:43 +0200

    3.9 - 3.11, deploy backend and frontend to the web

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fullstack3
+
+Urls
+
+Frontend
+```
+https://phonebook-be-23452345.fly.dev/
+```
+
+Backend
+```
+https://phonebook-be-23452345.fly.dev/api/persons
+```
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('postData', (req,res) => { return JSON.stringify(req.body)})
 const morganString = ':method :url :status :res[content-length] - :response-time ms :postData'
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(morganString))
 
@@ -82,7 +83,7 @@ app.post("/api/persons/", (req,res) => {
 })
 
 
-const PORT = 3001
+const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
